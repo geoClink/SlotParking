7b07ee2d6fe5e77afdb50d533f6fcfd0dd40ad65
George Clinkscales <1lclink2@att.net>
2026-03-16 17:29:34 -0400

    Initial Commit

---
+++ SlotParking/SlotParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotParkingApp.swift
+//  SlotParking
+//
+//  Created by George Clinkscales on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SlotParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
